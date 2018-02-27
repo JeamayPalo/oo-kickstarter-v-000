9c793475edaf46431e28eaa6a2d7876d8fe9b29e
Jeamay Palo <jeamay.palo@gmail.com>
2018-02-27 03:18:37 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -3,7 +3,13 @@ class Project
   attr_accessor :backers
 
   def initialize
+    @name = name 
+    @title = title 
     @backers = []
   end
+
+  def add_backer(Backer)
+    @backer << Backer 
+  end 
 
 end
